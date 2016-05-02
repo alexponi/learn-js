6d436022dd13972ee10e17a942f7042fbfc701f9
alexponi <alexponi@mail.ru>
2016-05-02 20:09:03 +0500

    add for and links

---
+++ js/for.js
@@ -0,0 +1,4 @@
+for (age = 3; age < 10; age++){
+	console.log("Your age is less 10");
+}
+document.write("You are now over 10");
---
+++ js/links.js
@@ -0,0 +1,5 @@
+var links = document.getElementsByTagName("a");
+for (var i = 1; i < links.length; i++){
+	console.log('this is link number ' + i);
+}
+document.write("all links now looped");
