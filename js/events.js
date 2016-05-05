0254adeafc5219b8db6e3b9b67873efb2d2fd0b2
alexponi <alexponi@mail.ru>
2016-05-06 00:17:58 +0500

    add remove and events

---
+++ js/events.js
@@ -0,0 +1,7 @@
+var title = document.getElementById("page-title");
+title.onclick = function () {
+  alert("you click me");
+};
+title.onmouseover = function () {
+  alert("you hovered your mouse over me");
+};
---
+++ js/remove.js
@@ -0,0 +1,4 @@
+var parent = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[0];
+var removed = parent.removeChild(child);
+parent.appendChild(removed);
