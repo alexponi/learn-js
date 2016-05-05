184f180eb8f2cd5cde1abcc529727ce44a89f4b8
alexponi <alexponi@mail.ru>
2016-05-05 22:24:02 +0500

    add DOM2

---
+++ js/DOM2.js
@@ -0,0 +1,10 @@
+// 1 method:
+var myBody = document.getElementsByTagName("body");
+myBody[0].innerHTML = "<p>I am a paragraph tag</p>";
+
+// 2 method:
+var myTitle = document.getElementById("page-title");
+myTitle.textContent = "Yo Ninjas, get ready to learn!";
+
+
+
