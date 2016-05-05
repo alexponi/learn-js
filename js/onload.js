1752d065451ed9543d9b5bfb5ebbaf69f646f048
alexponi <alexponi@mail.ru>
2016-05-06 00:49:33 +0500

    add onload

---
+++ js/onload.js
@@ -0,0 +1,17 @@
+function setUpEvents(){
+	var content = document.getElementById("content");
+	var button = document.document.getElementById("button");
+	button.onclick = function () {
+		if (content.classname == "open"){
+			content.className = "";
+			button.innerHTML = "Show More";
+		} else {
+			content.className = "open";
+			button.innerHTML = "Show Less";
+		}
+  };
+}
+
+window.onload = function () {
+	setUpEvents();
+}
